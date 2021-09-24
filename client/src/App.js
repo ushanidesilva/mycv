eac808ecb192fce0c57908e53a499a1f49cc6532
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-24 21:01:48 +0530

    Edit App.js - Header part

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import React, { Component } from 'react';
 import './App.css';
 import UserForm from './components/UserForm';
 
+import resume from './img/resume.png'
+
 class App extends Component {
 
   
   
   render() {
     return (
-      <div>
+      <div styles={{ backgroundImage:`url(${resume})` }} >
         <div className="col-lg-8 mx-auto text-center mt-5">
-          <h1><b>Let's generate your Resume!</b></h1>
-          <p className="lead">Please provide accurate and clear description wherever necessary.</p>
+        <img src={resume} width="100" height="100" />
+          <h1 style={{color: "red"}}><b>Create Your Resume Here</b></h1>
+          <p className="lead">Add your Details Here</p>
           <hr />
         </div>  
         <UserForm/>
