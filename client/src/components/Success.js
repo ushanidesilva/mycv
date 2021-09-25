7beab5e8d476f3fedf27625c49c04bbfd39b8b2d
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-25 22:32:26 +0530

    Success update

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -11,8 +11,10 @@ export default class Success extends Component {
       <div className="card animated bounceIn">
         <div className="card-body text-center pt-5 pb-5">
             <i className="fas fa-check-circle fa-7x text-success"></i>
-            <h2>Hang Tight while your Resume Downloads!</h2>
+            <h2>Congradulations!</h2>
+            <h4>You have successfully created your Resume</h4>
             <br/>
+            <h4>Your Resume will be Download shortly!</h4>
             
         </div>  
         
