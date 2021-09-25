f04243f3a0938ce39d617dc1ae9804f20992b074
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-25 21:54:30 +0530

    Update Educaton

--- client/src/components/Education.js
+++ client/src/components/Education.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import education from '../img/education.png'
 
 export default class Education extends Component {
 
@@ -16,10 +17,10 @@ export default class Education extends Component {
         const { values, handleChange } = this.props;
         return (
 
-            <div className="card animated fadeInLeft">
+            <div className="card animated fadeInLeft" style={{backgroundColor:'#c7e6eb'}}>
                 <div className="card-body">
-
-                    <h3 className="card-title">Education Info</h3>
+                <img src={education} width="70" height="70" />
+                    <h3 className="card-title">Step 4 : Education Information</h3>
                     <hr />
                 </div>
                 <form onSubmit={this.continue}>
@@ -30,25 +31,25 @@ export default class Education extends Component {
 
                     <div className="row col-lg-10 mx-auto">
                         <div className="col-lg-12 text-left">
-                            <h3><b><i className="fas fa-check-circle mr-1"></i>1</b></h3>
+                            <h3><b><i className="fas fa-check-circle mr-1"></i>University / College Information</b></h3>
                         </div>
                         <div className="col-lg-4 text-left">
-                            <label>College/University*</label>
+                            <label><b>College/University Name *</b></label>
                             <input type="text" name="edu1_school" className="form-control" defaultValue={values.status === 1 ? '' : values.edu1_school} onChange={handleChange} required />
                         </div>
                         <div className="col-lg-4 text-left">
-                            <label>Year*</label>
+                            <label><b>Graduated Year *</b></label>
                             <input type="text" name="edu1_year" className="form-control" defaultValue={values.status === 1 ? '' : values.edu1_year} onChange={handleChange} required/>
                         </div>
                         <div className="col-lg-4 text-left">
-                            <label>Qualification*</label>
+                            <label><b>Qualification *</b></label>
                             <input type="text" name="edu1_qualification" className="form-control" defaultValue={values.status === 1 ? '' : values.edu1_qualification} onChange={handleChange} required/>
                         </div>
 
                     </div>
                     <div className="row col-lg-10 mx-auto">
                         <div className="col-lg-12 text-left">
-                            <label>Description*</label>
+                            <label><b>Description *</b></label>
                             <input type="text" name="edu1_desc" className="form-control" defaultValue={values.status === 1 ? '' : values.edu1_desc} onChange={handleChange} required />
                         </div>
                     </div>
@@ -61,33 +62,33 @@ export default class Education extends Component {
 
                     <div className="row col-lg-10 mx-auto">
                         <div className="col-lg-12 text-left">
-                            <h3><b><i className="fas fa-check-circle mr-1"></i>2</b></h3>
+                            <h3><b><i className="fas fa-check-circle mr-1"></i>School Information</b></h3>
                             <hr/>
                         </div>
                         <div className="col-lg-4 text-left">
-                            <label>School</label>
+                            <label><b>School Name *</b></label>
                             <input type="text" name="edu2_school" className="form-control" defaultValue={values.status === 1 ? '' : values.edu2_school} onChange={handleChange} />
                         </div>
                         <div className="col-lg-4 text-left">
-                            <label>Year</label>
+                            <label><b>Year *</b></label>
                             <input type="text" name="edu2_year" className="form-control" defaultValue={values.status === 1 ? '' : values.edu2_year} onChange={handleChange} />
                         </div>
                         <div className="col-lg-4 text-left">
-                            <label>Qualification</label>
+                            <label><b>Qualification *</b></label>
                             <input type="text" name="edu2_qualification" className="form-control" defaultValue={values.status === 1 ? '' : values.edu2_qualification} onChange={handleChange} />
                         </div>
 
                     </div>
                     <div className="row col-lg-10 mx-auto">
                         <div className="col-lg-12 text-left">
-                            <label>Description</label>
+                            <label><b>Description *</b></label>
                             <input type="text" name="edu2_desc" className="form-control" defaultValue={values.status === 1 ? '' : values.edu2_desc} onChange={handleChange} />
                         </div>
                     </div>
                     <br />
                     <div className="container text-center">
-                        <button type="button" className="btn btn-info" onClick={this.back}><i className="fas fa-angle-left mr-1"></i>Back</button>
-                        <button type="submit" className="btn btn-info">Next<i className="fas fa-angle-right ml-1"></i></button>
+                        <button type="button" className="btn btn-black" onClick={this.back}><i className="fas fa-angle-left mr-1"></i>Back</button>
+                        <button type="submit" className="btn btn-red">Next<i className="fas fa-angle-right ml-1"></i></button>
                     </div>
                     <br />
 
