b6793a48d4ef58c489a4ba3762c32bcbcd1ee331
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-28 02:40:52 +0530

    Edit Project.test

--- client/src/components/test/Project.test.js
+++ client/src/components/test/Project.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Project from '../Project';
 import {querySelectorElement} from '@testing-library/dom';
 import {querySelector} from '@testing-library/jest-dom';
+import {getQueriesForElement} from '@testing-library/dom';
 
 
 
@@ -11,3 +12,14 @@ it('renders without crashing', () => {
   ReactDOM.render(<Project />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+test("renders are correct", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Project />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+
+  expect(root.querySelector("h3").textContent).toBe("Step 3 : Recent Project Information");
+
+});
+
+
