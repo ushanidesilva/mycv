4124e05bd8b32b820e35548208d55f9c04303632
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-28 02:23:27 +0530

    Edit Experience.test

--- client/src/components/test/Experience.test.js
+++ client/src/components/test/Experience.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Experience from '../Experience';
+import {getQueriesForElement} from '@testing-library/dom';
 import {querySelectorElement} from '@testing-library/dom';
 import {querySelector} from '@testing-library/jest-dom';
 
@@ -11,5 +12,52 @@ it('renders without crashing', () => {
   ReactDOM.render(<Experience />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+test("renders are correct", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Experience />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+
+  expect(root.querySelector("h3").textContent).toBe("Step 2 : Work Experience Information");
+
+});
+
+test("Company Name element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Experience />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Company Name*")).not.toBeNull();
+  
+});
+
+test("Position   element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Experience />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Position *")).not.toBeNull();
+  
+});
+
+
+test("Duration  Name element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Experience />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Duration *")).not.toBeNull();
+  
+});
+
+test("Description  element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Experience />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Description *")).not.toBeNull();
+  
+});
+
 
 
