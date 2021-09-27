9cf43d63070a189e6f6247c8e3ebd1cf8423f182
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-28 02:10:37 +0530

    Edit Education.test

--- client/src/components/test/Education.test.js
+++ client/src/components/test/Education.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Education from '../Education';
-import {querySelectorElement} from '@testing-library/dom';
+import {getQueriesForElement} from '@testing-library/dom';
 import {querySelector} from '@testing-library/jest-dom';
+import {render , fireEvent} from '@testing-library/react';
+
 
 
 
@@ -12,4 +14,50 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+test("renders are correct", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Education />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+
+  expect(root.querySelector("h3").textContent).toBe("Step 4 : Education Information");
+
+});
+
+test("Name element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Education />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("University Name *")).not.toBeNull();
+  
+});
+
+test("Graduated Year  element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Education />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Graduated Year *")).not.toBeNull();
+  
+});
+
+
+test("School Name element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Education />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("School Name *")).not.toBeNull();
+  
+});
+
+test("Year  element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Education />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Year *")).not.toBeNull();
+  
+});
 
