a5e5990348ac237cbb42546c39af45c8f1db6447
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-28 02:34:04 +0530

    Edit Extras.test

--- client/src/components/test/Extras.test.js
+++ client/src/components/test/Extras.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Extras from '../Extras';
 import {querySelectorElement} from '@testing-library/dom';
 import {querySelector} from '@testing-library/jest-dom';
+import {getQueriesForElement} from '@testing-library/dom';
 
 
 
@@ -11,3 +12,33 @@ it('renders without crashing', () => {
   ReactDOM.render(<Extras />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+test("renders are correct", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Extras />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+
+  expect(root.querySelector("h3").textContent).toBe("Step 5 : Add Extra Curricular Activities");
+
+});
+
+test("Languages element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Extras />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Languages (ex: English, Chinese)")).not.toBeNull();
+  
+});
+
+test("Hobbies / Interests   element test", () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Extras />, root);
+
+  const {getByText, getByLableText} = getQueriesForElement(root);
+  expect(getByText("Hobbies / Interests")).not.toBeNull();
+  
+});
+
+
+
