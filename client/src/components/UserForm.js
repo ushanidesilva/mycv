503e3bafe60c09dce3b856e4917f24f6ce57a127
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-27 22:15:43 +0530

    Edit User Form

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -87,56 +87,10 @@ class UserForm extends Component {
 
     handleChange = ({ target: { value, name } }) => this.setState({ [name]: value })
 
-    /*formSubmit = (e) => {
-        e.preventDefault();
 
-        const data = {
-            name: this.state.name,
-            email: this.state.email,
-            phone: this.state.phone,
-            linkedin: this.state.linkedin,
-            github: this.state.github,
-            skills: this.state.skills
-        }
-
-
-        axios.post('/create-pdf', data)
-            .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
-            .then((res) => {
-                const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
-
-                saveAs(pdfBlob, 'Resume.pdf');
-            });
-
-        e.target.reset();
-
-    } */
-
-    /*createAndDownloadPdf = () => {
-      axios.post('/create-pdf', this.state)
-           .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
-           .then((res) => {
-              const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
-  
-              saveAs(pdfBlob, 'Resume.pdf');
-           })
-    } */
     render() {
         const { step } = this.state;
-        // const { name, email, phone, linkedin, github, skills, exp1_org, exp1_pos, exp1_desc, exp1_dur,
-        //     exp2_org, exp2_pos, exp2_desc, exp2_dur, status, proj1_title, proj1_link, proj1_desc,
-        //     proj2_title, proj2_link, proj2_desc, edu1_school, edu1_year, edu1_qualification,
-        //     edu1_desc, edu2_school, edu2_year, edu2_qualification, edu2_desc, extra_1, extra_2,
-        //     extra_3, extra_4, extra_5 } = this.state;
-        // const values = {
-        //     name, email, phone, linkedin, github, skills, exp1_org, exp1_pos, exp1_desc, exp1_dur,
-        //     exp2_org, exp2_pos, exp2_desc, exp2_dur, edu1_school, edu1_year, edu1_qualification,
-        //     edu1_desc, edu2_school, edu2_year, edu2_qualification, edu2_desc, status, proj1_title,
-        //     proj1_link, proj1_desc,
-        //     proj2_title, proj2_link, proj2_desc,
-        //     extra_1, extra_2,
-        //     extra_3, extra_4, extra_5
-        // };
+        
 
         switch (step) {
             case 1:
@@ -181,7 +135,7 @@ class UserForm extends Component {
                             <Project
                                 values={this.state}
                                 prevStep={this.prevStep}
-                                /*submitted={this.submitted}*/
+                                
                                 nextStep={this.nextStep}
                                 handleChange={this.handleChange}
                             />
@@ -199,7 +153,7 @@ class UserForm extends Component {
                             <Education
                                 values={this.state}
                                 prevStep={this.prevStep}
-                                /*submitted={this.submitted}*/
+                                
                                 nextStep={this.nextStep}
                                 handleChange={this.handleChange}
                             />
